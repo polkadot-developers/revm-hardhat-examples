3a01fd8466e53f66e6e3027cca7f147704497268
albertov19 <64150856+albertov19@users.noreply.github.com>
2025-11-07 11:28:21 +0100

    use keystore

--- erc20-hardhat/hardhat.config.ts
+++ erc20-hardhat/hardhat.config.ts
@@ -25,7 +25,7 @@ const config: HardhatUserConfig = {
     polkadotTestnet: {
       type: "http",
       url: 'http://127.0.0.1:8545',
-      accounts: [process.env.PRIVATE_KEY || ''],
+      accounts: [configVariable("TESTNET_PRIVATE_KEY")],
     },
   },
 };
